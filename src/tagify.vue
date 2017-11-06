18716d22678f973e6616a9d5a97353ec2ef6ba7c
Simon Dann <carbontwelve@users.noreply.github.com>
2017-11-06 13:30:06 +0000

    :tada: Initial setup (#1)
    * Create package.json
    
    * Create .gitignore
    
    * Create tagify.vue

---
+++ src/tagify.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tagify"></div>
+</template>
+
+<script>
+  import * from '@yaireo/tagify'
+  export default {
+    name: 'tagify',
+    mounted() {
+      console.log('Tagify mounted.')
+    },
+    data: () => {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
