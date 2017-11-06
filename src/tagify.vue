9dfccc650f84c37d8b0768515487048d4a103b54
Simon Dann <carbontwelve@users.noreply.github.com>
2017-11-06 14:30:36 +0000

    :sparkles: Tagify vue.js component now works

--- src/tagify.vue
+++ src/tagify.vue
@@ -1,18 +1,67 @@
 <template>
-    <div class="tagify"></div>
+    <div class="tagify">
+        <input :name="name" :placeholder="placeholder" v-model="internalValue">
+    </div>
 </template>
 
 <script>
-  import * from '@yaireo/tagify'
+  const Tagify = require('@yaireo/tagify/dist/tagify');
+  let t;
   export default {
     name: 'tagify',
     mounted() {
-      console.log('Tagify mounted.')
+      this.internalValue = this.value
+
+      t = new Tagify(
+        document.querySelector('input[name='+this.name+']'), this.config
+      )
+
+      t.on('remove', this.updateInternalValue)
+      t.on('add', this.updateInternalValue)
+    },
+    beforeDestroy: function () {
+      t.destroy()
+    },
+    props: {
+      config:{
+        type: Object,
+        default: () => {
+          return {
+            delimiters: ',',
+            pattern: '',
+            duplicates: false,
+            enforeWhitelist: false,
+            autocomplete: true,
+            whitelist: [],
+            blacklist: [],
+            callbacks: {},
+            maxTags: Infinity,
+            suggestionsMinChars: 2
+          }
+        }
+      },
+      placeholder: String,
+      name: {
+        type: String,
+        required: true
+      },
+      value: Array
     },
     data: () => {
-      return {}
+      return {
+        internalValue: ''
+      }
+    },
+    watch: {
+      'internalValue': function() {
+        this.$emit('input', this.internalValue);
+      }
     },
     computed: {},
-    methods: {}
+    methods: {
+      updateInternalValue: function (e) {
+        this.internalValue = t.value
+      },
+    }
   }
 </script>
